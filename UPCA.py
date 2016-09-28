d908eb2f68104026e78e1e64217342c901c6c3a5
xtygah14three <xtygah14three@foxmail.com>
2016-09-29 00:17:47 +0800

    删除没必要的代码

--- UPCA.py
+++ UPCA.py
@@ -7,20 +7,6 @@ from os.path import expanduser
 import Templates
 
 
-class UserInfoDialog(QDialog):
-    def __init__(self):
-        super(UserInfoDialog,self).__init__()
-        self.setModal(True)
-    def closeEvent(self, QCloseEvent):
-        print("close")
-
-    def destroy(self, *args, **kwargs):
-        print('destory')
-        pass
-    def finished(self, p_int):
-        print('finished')
-        pass
-
 class MainWindow(QWidget):
     current_selected_plugin = ''
     current_selected_module = ''
@@ -50,7 +36,6 @@ class MainWindow(QWidget):
             print("找到用户配置文件 "+self.user_ini)
         else:
             dialog = QDialog()
-            #ok = QPushButton("确定",dialog)
             self.userinfoDialog = dialog
             dialog.setWindowTitle("输入信息")
             dialog.setFixedSize(500,500)
@@ -75,8 +60,6 @@ class MainWindow(QWidget):
             dialogLayout.addWidget(ok)
             ok.clicked.connect(self.update_user_info)
             dialog.exec_()
-            #f = open(self.user_ini,'w+')
-            #f.writelines("helo world")
             print("写入用户配置文件 "+self.user_ini)
         pass
     def update_user_info(self):
@@ -88,7 +71,7 @@ class MainWindow(QWidget):
             info = info.replace("$auhtorname$",author_name)
             info = info.replace("$authoremail$",author_email)
             info = info.replace("$sign$",author_sign)
-            f = open(self.user_ini,"w+")
+            f = open(self.user_ini, "w+", encoding='utf8')
             f.write(info)
             self.userinfoDialog.accept()
         else:
@@ -109,7 +92,6 @@ class MainWindow(QWidget):
         return self.pluginPCHDic[self.current_selected_module]
 
     def create_slate_class(self):
-        #print("create slate")
         if self.headerNameEdit.text()[0] != 'S':
             QMessageBox.about(self, "", "Slate的类必须以 S 开头")
             return
@@ -119,22 +101,17 @@ class MainWindow(QWidget):
                  + self.headerNameEdit.text()
         cpp_path = base_path + "\\" + self.cppFileLocCombox.currentText() + "\\" \
                    + self.cppNameEdit.text()
-
         h_str = h_str.replace("$classname$", self.classNameEdit.text())
         h_str = h_str.replace("$API$", self.get_header_api_marcro())
         h_str = h_str.replace("$authorinfo$", self.get_copyright_info_temp())
-
         h_write = open(h_path, 'w+', encoding='utf8')
         h_write.write(h_str)
-
         cpp_str = Templates.slate_cpp_template
         cpp_str = cpp_str.replace("$classname$", self.classNameEdit.text())
         cpp_str = cpp_str.replace("$pchfile$", self.get_pch_file())
         cpp_write = open(cpp_path, 'w+', encoding='utf8')
         cpp_write.write(cpp_str)
-
         QMessageBox.about(self, "", "创建成功")
-        pass
 
     def get_header_api_marcro(self):
         if self.add_module_api.isChecked():
@@ -149,7 +126,6 @@ class MainWindow(QWidget):
 
     def create_tree(self):
         view = QTreeWidget()
-
         view.itemPressed.connect(self.tree_select)
         view.setHeaderLabel("选择你要操作的模块")
         for i in self.pluginPathDic:
@@ -177,28 +153,23 @@ class MainWindow(QWidget):
         self.headerFileLocCombox.addItems(["Public", "Private", "Classes"])
         self.cppFileLocCombox = QComboBox()
         self.cppFileLocCombox.addItems(["Private", "Classes", "Public"])
-
         grid = QGridLayout()
         grid.addWidget(QLabel("名  称"), 0, 0)
         grid.addWidget(self.classNameEdit, 0, 1)
         grid.addWidget(QLabel("父类"), 0, 2)
         grid.addWidget(self.parentClassCombox, 0, 3)
-
         grid.addWidget(QLabel("头文件"), 1, 0)
         grid.addWidget(self.headerNameEdit, 1, 1)
         grid.addWidget(QLabel("位置"), 1, 2)
         grid.addWidget(self.headerFileLocCombox, 1, 3)
-
         grid.addWidget(QLabel("源文件"), 2, 0)
         grid.addWidget(self.cppNameEdit, 2, 1)
         grid.addWidget(QLabel("位置"), 2, 2)
         grid.addWidget(self.cppFileLocCombox, 2, 3)
         layout.addLayout(grid)
-
         self.add_module_api = QCheckBox("在类名前添加 XXX_API")
         # self.add_module_api.hide()
         layout.addWidget(self.add_module_api)
-
         self.generated_class_type = QComboBox()
         self.generated_class_type.addItems(["GENERATED_BODY", "GENERATED_UCLASS_BODY"])
         self.generate_box = QHBoxLayout()
@@ -206,11 +177,9 @@ class MainWindow(QWidget):
         self.generate_box.addWidget(self.generated_class_type)
         self.generate_box.addStretch()
         layout.addLayout(self.generate_box)
-
         self.classinfo = QTextEdit()
         self.classinfo.setText("类描述")
         layout.addWidget(self.classinfo)
-
         layout.addStretch()
         create_button = QPushButton("创建类")
         create_button.setFixedHeight(50)
@@ -228,7 +197,6 @@ class MainWindow(QWidget):
     def tree_select(self):
         self.current_selected_module = self.sender().selectedItems()[0].text(0)
         try:
-            #print(self.pluginModulePathDic[self.current_selected_module])
             public_api_str = self.current_selected_module.upper()
             self.add_module_api.setText("在类名前添加 " + public_api_str + "_API")
             self.add_module_api.show()
@@ -243,10 +211,6 @@ class MainWindow(QWidget):
 
     def code_analyse(self):
         self.mudules = []
-        current_path = os.getcwd()
-        parent_path = os.path.dirname(current_path)
-        #plugin_path = parent_path + "\plugins"
-        # 2.0版本
         plugin_path = os.getcwd()+"\\Plugins"
         plugin_name = ""
         module_name = ""
